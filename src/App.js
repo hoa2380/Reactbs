fbf7fefa60d40b3268102db85efa2a2674df0c94
phanvanhoa2380 <hoagamer2k@gmail.com>
2019-10-24 08:24:57 +0700

    component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import SinhVien from './components/SinhVien/SinhVien';
+import SanPham from './components/SanPham/SanPham';
 
 
 
@@ -32,11 +32,28 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1>Phan Văn Hoà</h1>
-        <p>Đang học</p>
-        <SinhVien hoten = "Phan Văn Hoà" tuoi = "19">
-          Sở Thích: Bóng đá, Chơi Game, Chân lí trong tim tôi,...
-        </SinhVien>
+        <h1>SẢN PHẨM</h1>
+        <SanPham tenSanPham = "Iphone 11" maSanPham = "1" giaSanPham = "30.000.000" >
+          Hàng Chính Hãng
+        </SanPham>
+        <p>==</p>
+        <SanPham tenSanPham = "Samsung Galaxy Note 10" maSanPham = "2" giaSanPham = "20.000.000" >
+          Hàng Xách Tay
+        </SanPham>
+        <p>==</p>
+        <SanPham tenSanPham = "Samsung Galaxy S10" maSanPham = "3" giaSanPham = "15.000.000" >
+          Hàng Chính Hãng
+        </SanPham>
+        <p>==</p>
+        <SanPham tenSanPham = "Iphone 11 Pro Max" maSanPham = "4" giaSanPham = "40.000.000" >
+          Hàng Chính Hãng
+        </SanPham>
+        <p>==</p>
+        <SanPham tenSanPham = "Iphone X" maSanPham = "5" giaSanPham = "17.000.000" >
+          Hàng Chính Hãng
+        </SanPham>
+        <p>==</p>
+
         
       </div>
     );
