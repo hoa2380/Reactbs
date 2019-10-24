3bc96e387884537e27ba501506a41ff0e062bf5f
phanvanhoa2380 <hoagamer2k@gmail.com>
2019-10-24 11:02:19 +0700

    hoc state

--- src/components/SinhVien/SinhVien.js
+++ src/components/SinhVien/SinhVien.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const inTuoi = () =>{
-    return Math.floor((Math.random() * 100));
-}
+
 
 const sinhVien = (props) =>{
     return(
         <div>
             <p>{ props.hoten } - Tuổi : { props.tuoi }</p>
-            <p> { props.children } </p>
+            <p> Sở thích : { props.sothich } </p>
             {/* <p>Hoàng Thị Tí - Tuổi: { inTuoi() }</p>
             <p>Hoàng Thị Xí - Tuổi : { inTuoi() }</p> */}
         </div>
